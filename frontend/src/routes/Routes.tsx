675401e9848807e89b0ffd3c9a62915e09082f58
Summation <wachirawichkanil@gmail.com>
2025-07-09 13:13:44 +0700

    remove more lazy imports

--- frontend/src/routes/Routes.tsx
+++ frontend/src/routes/Routes.tsx
@@ -1,14 +1,9 @@
-import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
 import { UserProvider } from '../contexts/UserContext';
 import { AdminProvider } from '../contexts/AdminContext';
 import NotFound from '../pages/errors/NotFound';
-import { Container, Box, Typography } from "@mui/material";
-import Navbar from "../components/Navbar";
-
-// Lazy load route modules
-const UserRoutes = lazy(() => import("./UserRoutes"));
-const AdminRoutes = lazy(() => import("./AdminRoutes"));
+import UserRoutes from "./UserRoutes";
+import AdminRoutes from "./AdminRoutes";
 
 const UserLayout = () => (
   <UserProvider>
@@ -23,24 +18,11 @@ const AdminLayout = () => (
 );
 
 const AppRoutes = () => (
-  <Suspense fallback={        
-    <>
-      <Navbar showButtons={false} />
-      <Container maxWidth="sm" sx={{ mt: 8 }}>
-          <Box textAlign="center">
-              <Typography variant="body1">
-                  Loading...
-              </Typography>
-          </Box>
-      </Container>
-    </>
-  }>
-    <Routes>
-      <Route path="/*" element={<UserLayout />} />
-      <Route path="/admin/*" element={<AdminLayout />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  </Suspense>
+  <Routes>
+    <Route path="/*" element={<UserLayout />} />
+    <Route path="/admin/*" element={<AdminLayout />} />
+    <Route path="*" element={<NotFound />} />
+  </Routes>
 );
 
 export default AppRoutes;
